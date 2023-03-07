c01cfa4b61d5007fa9a5ef5ec7c438c5c445e3b2
Growlheard <growlheard@gmail.com>
2023-03-08 01:17:19 +0300

    add 2 new classes

--- Channels.py
+++ Channels.py
@@ -1,4 +1,4 @@
-from main import Youtube
+from main import Youtube, Video, PLVideo
 
 channel_1 = Youtube('UCMCgOm8GZkHp8zJ6l7_hIuA')  # Вдудь
 channel_2 = Youtube('UC1eFXmJNkjITxPFWTy6RsWg')  # Редакция
@@ -11,3 +11,9 @@ total_subscribers = channel_1 < channel_2
 print(total_subscribers)
 total_subscribers = channel_1 > channel_2
 print(total_subscribers)
+
+
+video1 = Video('9lO06Zxhu88')
+video2 = PLVideo('BBotskuyw_M', 'PL7Ntiz7eTKwrqmApjln9u4ItzhDLRtPuD')
+
+print(video2)
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ class Youtube:
     """
    Работа с API Youtube
     """
+
     def __init__(self, channel_id):
         self._channel_id = channel_id
 
@@ -19,7 +20,7 @@ class Youtube:
         self._channel_id = self.channel['items'][0]['id']  # id канала
         self.channel_title = self.channel['items'][0]['snippet']['title']  # название канала
         self.channel_description = self.channel['items'][0]['snippet']['description']  # описание канала
-        self.channel_url = 'https://www.youtube.com/channel/' + self.channel_id   # ссыдка на канал
+        self.channel_url = 'https://www.youtube.com/channel/' + self.channel_id  # ссыдка на канал
         self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])  # количество подписчиков
         self.video_count = int(self.channel['items'][0]['statistics']['videoCount'])  # количество видео
         self.view_count = int(self.channel['items'][0]['statistics']['viewCount'])  # общее количество просмотров
@@ -45,7 +46,7 @@ class Youtube:
         api_key: str = os.environ.get('API_KEY')
         return build('youtube', 'v3', developerKey=api_key)
 
-    def to_json(self, filename) -> None:
+    def to_json(self, filename):
         """
         сохраняет информацию по каналу, хранящуюся в атрибутах экземпляра класса Youtube, в json-файл
         """
@@ -79,3 +80,56 @@ class Youtube:
         """
         if isinstance(other, Youtube):
             return self.subscriber_count > other.subscriber_count
+
+
+class Video:
+    def __init__(self, video_id):
+        self._video_id = video_id
+
+        youtube_service = Youtube.get_service()
+        self.video = youtube_service.videos().list(id=video_id, part='snippet,statistics').execute()
+
+        self.video_title = self.video['items'][0]['snippet']['title']
+        self.video_views = int(self.video['items'][0]['statistics']['viewCount'])
+        self.video_likes = int(self.video['items'][0]['statistics']['likeCount'])
+
+    @property
+    def video_id(self):
+        """
+        Возвращает id видео
+        """
+        return self.video_id
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self._video_id}')"
+
+    def __str__(self):
+        return f'{self.video_title}'
+
+    def video_info(self):
+        """
+        Выводим информацию о видео на консоль
+        """
+        print(json.dumps(self.video, indent=2, ensure_ascii=False))
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, video_playlist):
+        super().__init__(video_id)
+        self.video_playlist = video_playlist
+
+        youtube = Youtube.get_service()
+        self.playlist = youtube.playlists().list(id=self.video_playlist, part='snippet').execute()
+        self.playlist_title = self.playlist['items'][0]['snippet']['title']
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self._video_id}', '{self.video_playlist}')"
+
+    def __str__(self):
+        return f"{super().__str__()} ({self.playlist_title})"
+
+    def info_playlist(self):
+        """
+        Выводим информацию о плэйлисте в консоль
+        """
+        print(json.dumps(self.playlist, indent=2, ensure_ascii=False))
